d73f91de995f4f0e27c11fc172f4bf2dfc1b22ba
WDennington <willdennington@hotmail.co.uk>
2021-06-01 09:48:03 +0000

    added service 2 & 3

---
+++ 5001-name-class/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from os import getenv
+import random
+
+app = Flask(__name__)
+
+@app.route("/gen_name")
+def gen_name():
+    names = ['Al E. Gater', 'Funder Dew Drop', 'ah chew', 'chimpo shumpooli', 'Sam "The pie flinger" jones',
+            'wumbo toot', 'bibble', 'eggbert headington', 'droop', 'Lou Sirr']
+    return random.choice(names)
+
+
+
+@app.route("/gen_class")
+def gen_class():
+    classes = ['Barbarian', 'Bard', 'Cleric', 'Druid', 'Fighter', 'Monk', 'Paladin', 'Ranger', 'Rouge', 'Sorcerer', 'Warlock', 'Wizard', 'Artificer']
+    return random.choice(classes)
