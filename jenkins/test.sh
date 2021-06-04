fa4b13ef3d9a791848fca48d25d2a2607da925fd
WDennington <willdennington@hotmail.co.uk>
2021-06-04 11:20:19 +0000

    typo

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -12,7 +12,7 @@ source venv/bin/activate
 pip3 install -r jenkins/requirements.txt
 
 #run pytest
-python3 -m pytest 5000_templates --cov5000_templates --cov-report=html --junitxml=junit/test-results1.xml
-python3 -m pytest 5001_name_class --cov5001_name_class --cov-report=html --junitxml=junit/test-results2.xml
-python3 -m pytest 5002_stats --cov5002_stats --cov-report=html --junitxml=junit/test-results3.xml
-python3 -m pytest 5003_character --cov5003_character --cov-report=html --junitxml=junit/test-results4.xml
+python3 -m pytest 5000_templates --cov=5000_templates --cov-report=html --junitxml=junit/test-results1.xml
+python3 -m pytest 5001_name_class --cov=5001_name_class --cov-report=html --junitxml=junit/test-results2.xml
+python3 -m pytest 5002_stats --cov=5002_stats --cov-report=html --junitxml=junit/test-results3.xml
+python3 -m pytest 5003_character --cov=5003_character --cov-report=html --junitxml=junit/test-results4.xml
