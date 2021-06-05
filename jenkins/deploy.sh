5c21afcdaf8648cdf1969541769b366245004cd6
WDennington <willdennington@hotmail.co.uk>
2021-06-05 12:16:32 +0000

    changed ansible playbook

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -2,6 +2,6 @@ scp -o StrictHostKeyChecking=no docker-compose.yaml swarm-master:
 ssh swarm-master << EOF
 
 export DATABASE_URI=${DATABASE_URI}
-docker stack deploy --compose-file docker-compose.yaml
+docker stack deploy --compose-file docker-compose.yaml practical_dnd_project
 
 EOF
