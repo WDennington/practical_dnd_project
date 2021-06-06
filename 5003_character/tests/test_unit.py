985d7a04587332f66769f592e654f13e51cbb6ed
WDennington <willdennington@hotmail.co.uk>
2021-06-06 11:30:39 +0000

    testing v2

--- 5003_character/tests/test_unit.py
+++ 5003_character/tests/test_unit.py
@@ -9,25 +9,25 @@ class TestBase(TestCase):
 
 class TestHome(TestBase):
     def test_gen_char_dwarf(self):        
-        payload = dict(char_class='Dwarf',stats = [12, 13, 15, 6, 8, 10])
+        payload = dict(char_class='Dwarven Brute',stats = [12, 13, 15, 6, 8, 10])
         response = self.client.post(url_for('gen_char'), json=payload)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json,[12,13,17,6,8,10])
 
     def test_gen_char_elf(self):        
-        payload = dict(char_class='Elf',stats = [12, 13, 15, 6, 8, 10])
+        payload = dict(char_class='High Elf',stats = [12, 13, 15, 6, 8, 10])
         response = self.client.post(url_for('gen_char'), json=payload)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json,[12,15,15,6,8,10])
     
     def test_gen_char_gnome(self):        
-        payload = dict(char_class='Gnome',stats = [12, 13, 15, 6, 8, 10])
+        payload = dict(char_class='Gnomish Street Urchin',stats = [12, 13, 15, 6, 8, 10])
         response = self.client.post(url_for('gen_char'), json=payload)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json,[12,13,15,8,8,10])
     
     def test_gen_char_Human(self):        
-        payload = dict(char_class='Human',stats = [12, 13, 15, 6, 8, 10])
+        payload = dict(char_class='Human of Stormwind',stats = [12, 13, 15, 6, 8, 10])
         response = self.client.post(url_for('gen_char'), json=payload)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json,[13,14,16,7,9,11])
